99be1d407eb5e2bdce7ae6b60cdb48e52de786e6
Jefferson Santos <jefferson.pereira@ifood.com.br>
2023-03-02 20:07:13 -0300

    first commit plus hello world

---
+++ src/main/kotlin/com/manager/food/catalog/Application.kt
@@ -0,0 +1,12 @@
+package com.manager.food.catalog
+
+import io.ktor.server.application.*
+import com.manager.food.catalog.plugins.*
+
+fun main(args: Array<String>): Unit =
+    io.ktor.server.netty.EngineMain.main(args)
+
+@Suppress("unused") // application.conf references the main function. This annotation prevents the IDE from marking it as unused.
+fun Application.module() {
+    configureRouting()
+}
